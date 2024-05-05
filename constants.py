ec1852f08be7dc61fd25ce60f1b341df86849928
Sai Madhuri Kandula <saikandula@arizona.edu>
2024-05-05 13:46:35 -0700

    changed direction

--- constants.py
+++ constants.py
@@ -35,10 +35,10 @@ HEIGHT = 480
 
 # snake parameters
 class Direction(Enum):
-    RIGHT = 1
-    LEFT = 2
-    UP = 3
-    DOWN = 4
+    RIGHT = 'RIGHT'
+    LEFT = 'LEFT'
+    UP = 'UP'
+    DOWN = 'DOWN'
 
 Point = namedtuple('Point', 'x, y')
 
